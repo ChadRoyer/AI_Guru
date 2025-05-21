756d699c8535e8f5aed639f0d8e10286d3b13191
ChadRoyer <chad.j.royer@gmail.com>
2025-05-21 10:51:21 -0400

    Add basic layout

---
+++ components/ChatPanel.tsx
@@ -0,0 +1,5 @@
+export default function ChatPanel() {
+  return (
+    <div className="p-4">ChatPanel</div>
+  );
+}
---
+++ components/DiagramPanel.tsx
@@ -0,0 +1,5 @@
+export default function DiagramPanel() {
+  return (
+    <div className="p-4">DiagramPanel</div>
+  );
+}
