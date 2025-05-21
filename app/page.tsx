012ead7dda575c4c744e91a3afe227fc931b4e59
ChadRoyer <chad.j.royer@gmail.com>
2025-05-21 10:53:58 -0400

    Add page layout with Chat and Diagram panels

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import ChatPanel from '../components/ChatPanel';
+import DiagramPanel from '../components/DiagramPanel';
+
+export default function Page() {
+  return (
+    <div className="flex h-screen">
+      <div className="w-2/5 border-r overflow-y-auto">
+        <ChatPanel />
+      </div>
+      <div className="w-3/5 overflow-y-auto">
+        <DiagramPanel />
+      </div>
+    </div>
+  );
+}
---
+++ components/DiagramPanel.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DiagramPanel() {
+  return (
+    <div className="p-4">
+      {/* TODO: Implement diagram display logic */}
+      <p>Diagram will be rendered here.</p>
+    </div>
+  );
+}
